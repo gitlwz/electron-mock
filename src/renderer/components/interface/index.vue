db23a58d2815c95a8ea673fde6c9daedb9e48014
liuwenzhu <547485261@qq.com>
2019-02-22 09:23:23 +0800

    修改

--- src/renderer/components/interface/index.vue
+++ src/renderer/components/interface/index.vue
@@ -4,47 +4,60 @@
             <editor
                 @init="editorInit"
                 @input="changeData"
-                :autoComplete=true
-                :value='dataView'
-                :highlightActiveLine=true
+                :autoComplete="true"
+                :value="dataView"
+                :highlightActiveLine="true"
                 lang="json"
                 theme="monokai"
             ></editor>
         </div>
         <div class="right">
-            <div class="em-spots"><span
+            <div class="em-spots">
+                <span
                     class="decorate"
                     style="background: rgb(0, 62, 135); width: 44px; height: 44px; margin-top: -22px; margin-left: -22px; top: 11.9443%; left: 5%;"
-                ></span><span
+                ></span
+                ><span
                     class="decorate"
                     style="background: rgb(23, 90, 171); width: 34px; height: 34px; margin-top: -17px; margin-left: -17px; top: 17.7894%; left: 15%;"
-                ></span><span
+                ></span
+                ><span
                     class="decorate"
                     style="background: rgb(0, 62, 135); width: 36px; height: 36px; margin-top: -18px; margin-left: -18px; top: 69.9149%; left: 25%;"
-                ></span><span
+                ></span
+                ><span
                     class="decorate"
                     style="background: rgb(57, 38, 115); width: 41px; height: 41px; margin-top: -20.5px; margin-left: -20.5px; top: 74.5591%; left: 35%;"
-                ></span><span
+                ></span
+                ><span
                     class="decorate"
                     style="width: 43px; height: 43px; margin-top: -21.5px; margin-left: -21.5px; top: 65.6633%; left: 45%;"
-                ></span><span
+                ></span
+                ><span
                     class="decorate"
                     style="width: 36px; height: 36px; margin-top: -18px; margin-left: -18px; top: 43.6441%; left: 55%;"
-                ></span><span
+                ></span
+                ><span
                     class="decorate"
                     style="background: rgb(23, 90, 171); width: 37px; height: 37px; margin-top: -18.5px; margin-left: -18.5px; top: 84.8327%; left: 65%;"
-                ></span><span
+                ></span
+                ><span
                     class="decorate"
                     style="background: rgb(233, 34, 36); width: 34px; height: 34px; margin-top: -17px; margin-left: -17px; top: 97.7061%; left: 75%;"
-                ></span><span
+                ></span
+                ><span
                     class="decorate"
                     style="background: rgb(233, 34, 36); width: 42px; height: 42px; margin-top: -21px; margin-left: -21px; top: 86.3251%; left: 85%;"
-                ></span><span
+                ></span
+                ><span
                     class="decorate"
                     style="background: rgb(233, 34, 36); width: 26px; height: 26px; margin-top: -13px; margin-left: -13px; top: 23.2159%; left: 95%;"
-                ></span></div>
+                ></span>
+            </div>
             <div class="form-box">
-                <div class="title">{{this.formLabelAlign.id?"修改接口":"创建接口"}}</div>
+                <div class="title">
+                    {{ this.formLabelAlign.id ? "修改接口" : "创建接口" }}
+                </div>
                 <el-card class="box-card">
                     <el-form
                         label-position="top"
@@ -53,10 +66,7 @@
                         :rules="rules"
                         ref="dynamicValidateForm"
                     >
-                        <el-form-item
-                            label="Method"
-                            prop="method"
-                        >
+                        <el-form-item label="Method" prop="method">
                             <el-select
                                 v-model="formLabelAlign.method"
                                 placeholder="选择请求方法"
@@ -65,28 +75,46 @@
                                     label="POST"
                                     value="POST"
                                 ></el-option>
+                                <el-option label="GET" value="GET"></el-option>
                                 <el-option
-                                    label="GET"
-                                    value="GET"
+                                    label="websocket"
+                                    value="websocket"
                                 ></el-option>
                             </el-select>
                         </el-form-item>
-                        <el-form-item
-                            label="URL"
-                            prop="url"
-                        >
+                        <el-form-item label="URL" prop="url">
                             <el-input
                                 v-model="formLabelAlign.url"
                                 placeholder="输入请求地址"
                             >
-                                <template slot="prepend">/</template>
+                                <template slot="prepend"
+                                    >/</template
+                                >
                             </el-input>
                         </el-form-item>
+                        <div v-show="false">123</div>
                         <el-form-item
-                            label="描述"
-                            prop="describe"
+                            v-if="formLabelAlign.method === 'websocket'"
+                            label="推送频率(毫秒)"
+                            prop="time"
+                            :rules="[
+                                {
+                                    required: true,
+                                    message: '请输入推送频率',
+                                    trigger: 'change'
+                                }
+                            ]"
                         >
-                            <el-input v-model="formLabelAlign.describe"></el-input>
+                            <el-input-number
+                                v-model="formLabelAlign.time"
+                                controls-position="right"
+                                :min="10"
+                            ></el-input-number>
+                        </el-form-item>
+                        <el-form-item label="描述" prop="describe">
+                            <el-input
+                                v-model="formLabelAlign.describe"
+                            ></el-input>
                         </el-form-item>
                     </el-form>
                     <div>
@@ -95,7 +123,10 @@
                                 @click="submitForm('dynamicValidateForm')"
                                 type="primary"
                                 size="small"
-                            >{{this.formLabelAlign.id?"修改":"创建"}}</el-button>
+                                >{{
+                                    this.formLabelAlign.id ? "修改" : "创建"
+                                }}</el-button
+                            >
                             <el-button
                                 @click="formatJsonData(dataView)"
                                 size="small"
@@ -137,21 +168,21 @@ export default {
                     {
                         required: true,
                         message: "请选择方法类型",
-                        trigger: "blur"
+                        trigger: "change"
                     }
                 ],
                 url: [
                     {
                         required: true,
                         message: "请输入接口地址",
-                        trigger: "blur"
+                        trigger: "change"
                     }
                 ],
                 describe: [
                     {
                         required: true,
                         message: "请输入接口描述",
-                        trigger: "blur"
+                        trigger: "change"
                     }
                 ]
             }
